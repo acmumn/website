614d6774a35361bbb0e9c51521020d4f806fd190
nyadiia <nyadiia@pm.me>
2024-07-25 16:31:30 -0500

    build: fix config issue and update github actions

--- flake.nix
+++ flake.nix
@@ -4,18 +4,30 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
         devShell = pkgs.mkShell { buildInputs = with pkgs; [ zola ]; };
         packages.site = pkgs.stdenv.mkDerivation {
           name = "site";
           src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
 
           buildInputs = with pkgs; [ zola ];
-          phases = [ "unpackPhase" "buildPhase" ];
+          phases = [
+            "unpackPhase"
+            "buildPhase"
+          ];
           buildPhase = "zola build -o $out";
         };
-      });
+      }
+    );
 }
