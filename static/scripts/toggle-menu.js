53ee5d96c5b0799d37e1425a97fa045420520e21
arbormoss <arbormoss@woodsprite.dev>
2025-12-18 18:08:08 -0600

    feat: mobile menu

---
+++ static/scripts/toggle-menu.js
@@ -0,0 +1,19 @@
+let isMenuOpen = false;
+
+function toggleMenu() {
+    isMenuOpen = !isMenuOpen;
+    updateMenu();
+}
+
+function updateMenu() {
+    if (isMenuOpen) {
+        $('.navbar').children('a').css('display', 'grid');
+        $('.section:nth-child(0)').css('height', '100px');
+        return;
+    }
+
+    $('.navbar').children('a').css('display', 'none');
+    $('.section:nth-child(0)').css('height', '800px');
+}
+
+$(document).ready(updateMenu);
