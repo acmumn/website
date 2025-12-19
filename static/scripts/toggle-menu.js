e250cbe37ed6480f648627a1ce50b01056ecdbfe
arbor <arbormoss@woodsprite.dev>
2025-12-19 16:27:09 -0500

    Merge pull request #48 from acmumn/mobile-menu
    Mobile Menu (#52)
    
    * feat: add meeting minutes and link from events page
    
    * style: unify meeting minutes names
    
    * Fix Font Import (#44)
    
    * fix: import libre-franklin instead of using static assets
    
    * feat: remove unneeded fonts from font import
    
    * fix: use statically served font instead of google api
    
    * fix: remove unneded fonts
    
    * more contrast with bg for acm umn newsletter cards
    
    * feat: mobile menu
    
    * fix: correct lighthouse violations. (#45)
    
    * feat: add member link (#43)
    
    * fix: correct lighthouse violations.
    
    * chore: clarify what a esp32 is
    
    * style fix: update home page headings to pass lighthouse
    
    * fix: role="main" for <section> and title="Google Calendar" for google calendar embed
    
    ---------
    
    Co-authored-by: arbor <arbormoss@woodsprite.dev>
    Co-authored-by: june-fish <git@june.fish>
    
    * prog: mobile menu
    
    * fix: overflow on mobile nav
    
    * Fix Font Import (#44)
    
    * fix: import libre-franklin instead of using static assets
    
    * feat: remove unneeded fonts from font import
    
    * fix: use statically served font instead of google api
    
    * fix: remove unneded fonts
    
    * more contrast with bg for acm umn newsletter cards
    
    * feat: add meeting minutes and link from events page
    
    * style: unify meeting minutes names
    
    * fix: correct lighthouse violations. (#45)
    
    * feat: add member link (#43)
    
    * fix: correct lighthouse violations.
    
    * chore: clarify what a esp32 is
    
    * style fix: update home page headings to pass lighthouse
    
    * fix: role="main" for <section> and title="Google Calendar" for google calendar embed
    
    ---------
    
    Co-authored-by: arbor <arbormoss@woodsprite.dev>
    Co-authored-by: june-fish <git@june.fish>
    
    * fix: overflow on mobile nav
    
    * feat: mobile menu
    
    * feat: make mobile bar close automatically for mobile
    
    * fix: navbar when switching between mobile and desktop
    
    * fix: mobile nav height
    
    * refac: switch to static jquery
    
    * minor refac: remove unneeded pub key
    
    * refac: switch menu toggle to use pure js instead of jquery
    
    * fix: add aria-label to navbar hamburger menu
    
    ---------
    
    Co-authored-by: june-fish <git@june.fish>
    Co-authored-by: jastintime <130672671+jastintime@users.noreply.github.com>

---
+++ static/scripts/toggle-menu.js
@@ -0,0 +1,35 @@
+let isMenuOpen = false;
+
+function toggleMenu() {
+    isMenuOpen = !isMenuOpen;
+    updateNavbar();
+}
+
+function updateNavbar() {
+    const children = document.getElementById('navbar').children;
+    let display = 'grid';
+
+    // not on a phone
+    if (window.innerWidth > 580) {
+        document.getElementById('navbar').style.height = '100px';
+
+        for (let i = 0; i < children.length; i++) {
+            if (i == 0) { continue; }
+            children[i].style.display = display;
+        }
+        return;
+    }
+
+    document.getElementById('navbar').style.height = 'fit-content';
+
+    if (!isMenuOpen) {
+        display = 'none';
+    }
+    for (let i = 0; i < children.length; i++) {
+        if (i == 0) { continue; }
+        children[i].style.display = display;
+    }
+}
+
+document.onreadystatechange = updateNavbar;
+window.addEventListener("resize", updateNavbar);
